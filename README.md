e8724507515069f557aaf36baf0ea9e0ee2a75fd
Perry <perryzhu2004@outlook.com>
2024-09-06 10:41:01 -0700

    fix: dotenv not functioning

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Hello, this is my website.  
 Please visit my GitHub homepage to see more. 
 If you want to discuss anything with me, please email me at [perryzhu2004@outlook.com](mailto:perryzhu2004@outlook.com). 
+
+## `.env` setup 
+```shell
+# /backend/.env
+PORT=... # Port number
+```
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import 'dotenv/config'; // only wants the side effects 
 import router from './router.js';
 
 const app = express();
-dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
