43a6cfc1f33987bbad6909c758300059c9106cff
Perry <perryzhu2004@outlook.com>
2025-04-29 16:49:13 -0700

    feat: setup hero ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Alert, HeroUIProvider } from "@heroui/react";
 function App() {
   return (
-    <>
-      <h1 className="text-center text-3xl font-bold">Hello World</h1>
-    </>
+    <HeroUIProvider>
+      {/* <h1 className="text-center text-3xl font-bold">Hello World</h1> */}
+      <Alert title="Hello World" />
+    </HeroUIProvider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import { heroui } from "@heroui/react";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 };
