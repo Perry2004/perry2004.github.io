60b7d4bda55b7472ee54e98ca71a9cc98492d2e3
Perry <perryzhu2004@outlook.com>
2025-04-29 19:14:57 -0700

    feat: layout for home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,64 @@
+import { Button, Link } from "@heroui/react";
+import { SiGithub, SiLinkedin, SiInstagram, SiPexels } from "react-icons/si";
 import { NavbarPlaceholder } from "../layout";
+import { RollingImages } from "../ui";
+
+interface SocialMediaLink {
+  text: string;
+  href: string;
+  icon: React.ReactNode;
+}
 
 export function Home() {
+  const socialMediaLinks: SocialMediaLink[] = [
+    {
+      text: "GitHub",
+      href: "https://github.com/Perry2004",
+      icon: <SiGithub />,
+    },
+    {
+      text: "LinkedIn",
+      href: "https://www.linkedin.com/in/perry-z/",
+      icon: <SiLinkedin />,
+    },
+    {
+      text: "Instagram",
+      href: "https://www.instagram.com/perryzhu2004/",
+      icon: <SiInstagram />,
+    },
+    {
+      text: "Pexels",
+      href: "https://www.pexels.com/@perry-z-1662054943/",
+      icon: <SiPexels />,
+    },
+  ];
   return (
-    <div className="min-h-screen bg-green-100">
+    <div className="flex min-h-screen flex-col justify-evenly bg-green-100">
       <NavbarPlaceholder />
-      Home
+      <div className="ml-40 w-2/3 bg-green-400">
+        <h1 className="text-2xl font-bold">Hello World</h1>
+        <p className="text-xl">
+          Hi, I'm Perry Zhu, an international undergraduate majoring in Computer
+          Science and Statistics at the University of British Columbia (UBC).
+          <br />
+          <br />
+          I'm passionate about software development, machine learning, computer
+          graphics, and photography. Explore my projects, photos, and
+          experiences here!
+        </p>
+        <div className="mt-4 flex gap-2">
+          {socialMediaLinks.map((link) => {
+            return (
+              <Link href={link.href} key={link.text}>
+                <Button>
+                  {link.icon} {link.text}
+                </Button>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+      <RollingImages />
     </div>
   );
 }
---
+++ src/components/ui/RollingImages.tsx
@@ -0,0 +1,3 @@
+export function RollingImages() {
+  return <div className="h-52 w-full bg-pink-200">Rolling Images</div>;
+}
