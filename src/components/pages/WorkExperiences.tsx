a38b918d28d9c0d5e2337278734ddb9a03d162fe
Perry <perryzhu2004@outlook.com>
2025-04-30 22:25:10 -0700

    feat: display work experiences as cards along a timeline

--- src/components/pages/WorkExperiences.tsx
+++ src/components/pages/WorkExperiences.tsx
@@ -6,19 +6,22 @@ export function WorkExperiences() {
   return (
     <div className="min-h-screen bg-purple-200">
       <NavbarPlaceholder />
-      <Chrono
-        items={workExperiences}
-        mode="VERTICAL_ALTERNATING"
-        disableToolbar={true}
-        disableInteraction={true}
-        cardHeight={300}
-        useReadMore={true}
-        className="ul-default"
-        classNames={{
-          card: "w-auto h-auto",
-        }}
-        parseDetailsAsHTML={true}
-      />
+      <div className="ul-default">
+        <Chrono
+          items={workExperiences}
+          mode="VERTICAL_ALTERNATING"
+          disableToolbar={true}
+          disableInteraction={true}
+          cardHeight={300}
+          useReadMore={true}
+          classNames={{
+            card: "my-custom-card-class !min-h-0",
+            cardTitle: "my-custom-title-class",
+            cardText: "my-custom-text-class max-h-sub-clear !max-h-fit",
+          }}
+          parseDetailsAsHTML={true}
+        />
+      </div>
     </div>
   );
 }
