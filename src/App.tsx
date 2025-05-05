a351c21877c91d62461aa6900fc9d68b357fa030
Perry <perryzhu2004@outlook.com>
2025-05-04 19:47:00 -0700

    feat: click spark

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components";
 import ReactFullpage from "@fullpage/react-fullpage";
 import { ThemeProvider } from "@/context";
+import ClickSpark from "@/blocks/Animations/ClickSpark/ClickSpark";
 
 export function App() {
   const anchors = [
@@ -23,43 +24,52 @@ export function App() {
   return (
     <ThemeProvider>
       <HeroUIProvider>
-        <div className="font-raleway">
-          <NavbarHeader anchors={anchors} />
-          <ReactFullpage
-            anchors={anchors}
-            navigation={true}
-            scrollOverflow={true}
-            credits={{
-              enabled: false,
-              label: "",
-              position: "right",
-            }}
-            render={() => {
-              return (
-                <ReactFullpage.Wrapper>
-                  <div className="section">
-                    <Home />
-                  </div>
-                  <div className="section">
-                    <AboutMe />
-                  </div>
-                  <div className="section">
-                    <Projects />
-                  </div>
-                  <div className="section">
-                    <WorkExperiences />
-                  </div>
-                  <div className="section">
-                    <Skills />
-                  </div>
-                  <div className="section">
-                    <Contacts />
-                  </div>
-                </ReactFullpage.Wrapper>
-              );
-            }}
-          />
-        </div>
+        <ClickSpark
+          sparkColor="#fff"
+          sparkSize={20}
+          sparkRadius={40}
+          sparkCount={8}
+          duration={400}
+          extraScale={1.0}
+        >
+          <div className="font-raleway">
+            <NavbarHeader anchors={anchors} />
+            <ReactFullpage
+              anchors={anchors}
+              navigation={true}
+              scrollOverflow={true}
+              credits={{
+                enabled: false,
+                label: "",
+                position: "right",
+              }}
+              render={() => {
+                return (
+                  <ReactFullpage.Wrapper>
+                    <div className="section">
+                      <Home />
+                    </div>
+                    <div className="section">
+                      <AboutMe />
+                    </div>
+                    <div className="section">
+                      <Projects />
+                    </div>
+                    <div className="section">
+                      <WorkExperiences />
+                    </div>
+                    <div className="section">
+                      <Skills />
+                    </div>
+                    <div className="section">
+                      <Contacts />
+                    </div>
+                  </ReactFullpage.Wrapper>
+                );
+              }}
+            />
+          </div>
+        </ClickSpark>
       </HeroUIProvider>
     </ThemeProvider>
   );
