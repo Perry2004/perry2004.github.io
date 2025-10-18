189f594bb221481cc6429133df55997418d96e13
Perry Zhu <perryzhu2004@outlook.com>
2025-10-18 16:30:06 -0700

    feat: container-based lambda function

--- scripts/prepare-image-links.sh
+++ scripts/prepare-image-links.sh
@@ -4,6 +4,7 @@ if [ -f "public/data/rolling-images.json" ]; then
     exit 0
 fi
 cd scripts || exit
-yarn install
-yarn build
+npm install
+npm run build
+npx playwright install chromium
 node dist/main.js "../public/data/rolling-images.json"
